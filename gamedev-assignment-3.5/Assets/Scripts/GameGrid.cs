984e75f1e71cd1ec30914f96302fb432b278ed8f
unknown <jztang8@gmail.com>
2019-04-17 13:11:43 -0400

    Implement starting grid instantiation and tile darken on mouse hover

---
+++ gamedev-assignment-3.5/Assets/Scripts/GameGrid.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameGrid : MonoBehaviour {
+    public GameObject[,] tiles; // Matrix that represents the hexagonal grid
+    public GameObject tilePrefab;
+
+    public Text whiteScore;
+    public Text blackScore;
+
+
+
+    private void Start() {
+        tiles = new GameObject[11, 11];
+        GenerateGrid();
+    }
+
+    private void Update() {
+        
+    }
+
+    private Vector2 GetPosByIndex(int row, int col) {
+        float x = -3.9f + (0.78f * col);
+        float y = 4.05f - (0.9f * row);
+        if(col % 2 == 1) y += 0.45f;
+        return new Vector2(x, y);
+    }
+
+    private void GenerateGrid() {
+        for(int row = 0; row < 11; row++) {
+            for(int col = 0; col < 11; col++) {
+                GameObject newTile = Instantiate(tilePrefab);
+                newTile.transform.position = GetPosByIndex(row, col);
+                newTile.GetComponent<Tile>().SetTile(2);
+                tiles[row, col] = newTile;
+            }
+        }
+
+        int[] rowTemp = {0, 1, 8, 9, 10, 0, 1, 9, 10, 0, 9, 10, 0, 10, 10, 
+                         10, 0, 10, 0, 9, 10, 0, 1, 9, 10, 0, 1, 8, 9, 10};
+        int[] colTemp = {0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 
+                         6, 7, 7, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10};
+
+        for(int i = 0; i < rowTemp.Length; i++) {
+            Destroy(tiles[rowTemp[i], colTemp[i]]);
+        }
+
+        tiles[4, 4].GetComponent<Tile>().SetTile(0);
+        tiles[6, 5].GetComponent<Tile>().SetTile(0);
+        tiles[4, 6].GetComponent<Tile>().SetTile(0);
+
+        tiles[5, 4].GetComponent<Tile>().SetTile(1);
+        tiles[4, 5].GetComponent<Tile>().SetTile(1);
+        tiles[5, 6].GetComponent<Tile>().SetTile(1);
+    }
+}
---
+++ gamedev-assignment-3.5/Assets/Scripts/Tile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+    public int type;
+    public Color[] colors;
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+    public void SetTile(int t) {
+        type = t;
+        GetComponent<SpriteRenderer>().color = colors[type];
+    }
+
+    void OnMouseOver() {
+        if(type == 2) {
+            this.SetTile(3);
+        }
+    }
+
+    void OnMouseExit() {
+        if(type == 3) {
+            this.SetTile(2);
+        }
+    }
+}
